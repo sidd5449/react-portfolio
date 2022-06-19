e9df99fdc6a9bcd2e4d692f71fb01a6a39d05d45
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2022-06-19 23:23:25 +0530

    changes made to theming of the site

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -44,14 +44,15 @@ const Header = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg" />
-      <motion.img
+      <img src={images.cover} alt="profile_bg" />
+      {/* <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}
         src={images.circle}
+        
         alt="profile_circle"
         className="overlay_circle"
-      />
+      /> */}
     </motion.div>
 
     <motion.div
